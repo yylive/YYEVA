d36887f891e57b6222602c18522f342694a25e57
guoyabin2 <guoyabin2@yy.com>
2022-12-08 15:44:39 +0800

    修改cep-demo案例的host.jsx

--- AEP/cep-base/jsx/host.jsx
+++ AEP/cep-base/jsx/host.jsx
@@ -1,11 +1,22 @@
 
  
 /**
- * 用来生成支持MP4视频插入动态元素的AE脚本
+ * 获取当前所选合成的名字
  * Author:Guoyabin YY Inc
  */
  
- var confirmMessage = function (message) {
-    return confirm(message);
+ function getActivieItemName(){
+   var activeItem = app.project.activeItem
+   var name = activeItem.name
 }
-  
+ 
+ 
+/**
+ * 获取当前所选合成的第一个图层的名字
+ * Author:Guoyabin YY Inc
+ */
+function getActivieLayers(){
+   var activeItem = app.project.activeItem
+   var layer = activeItem.layer[1]
+   var layerName = layer.name
+}
